3ef04d0e74a405d171e8f39ca0af8e693e4ccb76
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-23 17:27:54 -0400

    Display permutations of dice

--- permutations.rb
+++ permutations.rb
@@ -1,3 +1,5 @@
-possibilities = (1..6).to_a
+die1 = (1..6).to_a
+die2 = (1..6).to_a
+combined_die = die1.repeated_permutation(2).to_a
 
-puts possibilities.repeated_permutation(2).to_a
+combined_die.each_index{|i| puts "#{combined_die[i]}" }
