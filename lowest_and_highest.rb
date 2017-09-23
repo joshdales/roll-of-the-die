da75dc67bba02ae10cd394bb9a76b49ff073e1b5
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-23 10:26:45 -0400

    Display lowest and highest values along with the results

--- lowest_and_highest.rb
+++ lowest_and_highest.rb
@@ -1,14 +1,14 @@
 sort_roll = []
 
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
-sort_roll << " The rusult of your roll is #{Random.rand(1..6)}"
+5.times do
+  sort_roll << Random.rand(1..6)
+end
 
-puts sort_roll.sort!
+sort_roll.sort!
+
+sort_roll.each do |roll|
+  puts "The rusult of your roll is #{roll}"
+end
+
+puts "The lowest number is #{sort_roll.first}."
+puts "The highest number is #{sort_roll.last}."
