bda14431eb24d8ac721a14b5e52bc54e42343e97
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-23 17:37:36 -0400

    Display permutations of dice and their totals

---
+++ permutation_totals.rb
@@ -0,0 +1,4 @@
+dice = (1..6).to_a
+dice_rolls = dice.repeated_permutation(2).to_a
+
+dice_rolls.each_index{ |i| puts "Dice Roll: #{dice_rolls[i]} Total: #{dice_rolls[i][0] + dice_rolls[i][1]}" }
--- permutations.rb
+++ permutations.rb
@@ -2,4 +2,5 @@ die1 = (1..6).to_a
 die2 = (1..6).to_a
 combined_die = die1.repeated_permutation(2).to_a
 
-combined_die.each_index{|i| puts "#{combined_die[i]}" }
+combined_die.each_index{ |i| puts "#{combined_die[i]}" }
+  
